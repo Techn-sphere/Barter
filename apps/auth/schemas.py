fd4646c0ca13c043cc448dd7329835c5e8feea03
George Rodionov <111501208+george2066@users.noreply.github.com>
2025-11-29 14:32:01 +0300

    create: auth and pydantic schemas

---
+++ apps/auth/schemas.py
@@ -0,0 +1,29 @@
+import uuid
+import datetime
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, StringConstraints
+
+
+class GetUserByID(BaseModel):
+    id: uuid.UUID | str
+
+
+class GetUserByEmail(BaseModel):
+    email: EmailStr
+
+
+class RegisterUser(GetUserByEmail):
+    password: Annotated[str, StringConstraints(min_length=8, max_length=128)]
+
+
+class CreateUser(GetUserByEmail):
+    hashed_password: str
+
+
+class UserReturnData(GetUserByID, GetUserByEmail):
+    is_active: bool
+    is_verified: bool
+    is_superuser: bool
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
--- apps/core/settings.py
+++ apps/core/settings.py
@@ -1,11 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from apps.core_dependency.dependencies import DBSettings
 
 
 class Settings(BaseSettings):
-    db_settings: DBSettings = DBSettings()  # Экземпляр класса DBSettings
+    db_settings: DBSettings = DBSettings()
+    secret_key: SecretStr
 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf8",
+        extra="ignore"
+    )
 
-# Инициализация переменной settings
 settings = Settings()
